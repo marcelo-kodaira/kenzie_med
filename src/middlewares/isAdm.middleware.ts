bb1699d30468a8fed495947fd1311fae5d2a0ef1
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2022-11-01 19:51:37 -0300

    Add: IsAdmin Middleware and Auth Middleware

---
+++ src/middlewares/isAdm.middleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../Error/AppError";
+
+const isAdmMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.user.isAdm) {
+    next();
+  }
+
+  throw new AppError("User is not an Administrator", 401);
+};
+
+export default isAdmMiddleware;
