9f8f3cda47fb84244327b0015f5931e05f6bfe7a
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2022-11-03 16:00:37 -0300

    Add: Ticket-Pastas

---
+++ src/interfaces/schedule/index.ts
@@ -0,0 +1,21 @@
+
+export interface IScheduleRequest {
+    hour: Date
+    date: Date
+    type: string
+    description: string
+    doctorsID: string
+}
+
+export interface ISchedule {
+    id: string
+    hour: Date
+    date: Date
+    type: string
+    description: string
+    userId: string
+    doctorId: string
+    createdAt: Date
+    updatedAt: Date
+}
+
