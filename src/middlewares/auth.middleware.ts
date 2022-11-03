55a7d521a82b8b5106a3deafdbb823520bc10780
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2022-11-03 09:45:17 -0300

    FIX: Correction of entities and folders names

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-import { AppError } from "../Error/AppError";
+import AppError from "../error/AppError";
 
 const autMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
--- src/middlewares/isAdm.middleware.ts
+++ src/middlewares/isAdm.middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "../Error/AppError";
+import AppError from "../error/AppError";
 
 const isAdmMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (req.user.isAdm) {
