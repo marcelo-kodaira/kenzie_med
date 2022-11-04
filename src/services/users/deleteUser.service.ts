404f36ef7a5af5f65011726c1568bc0f880c564a
Daniel Galvan <dgalvan@gmail.com>
2022-11-04 08:46:33 -0300

    Fix: Ticket001 - Services deleteUser e updateUser resolvidos

--- src/services/users/deleteUser.service.ts
+++ src/services/users/deleteUser.service.ts
@@ -1,9 +1,11 @@
 import AppDataSource from "../../data-source";
+import Addresses from "../../entities/address.entity";
 import Users from "../../entities/user.entity";
 import AppError from "../../Error/AppError";
 
 const deleteUserService = async (userId: string): Promise<void> => {
     const userRepository = AppDataSource.getRepository(Users);
+    const addressesRepository = AppDataSource.getRepository(Addresses);
     const findUser = await userRepository.findOneBy({ id: userId });
     
     if (!findUser) {
@@ -11,6 +13,7 @@ const deleteUserService = async (userId: string): Promise<void> => {
     }
 
     await userRepository.delete(userId)
+    await addressesRepository.delete(findUser.address.id)
 }
 
 export default deleteUserService;
--- src/services/users/updateUser.service.ts
+++ src/services/users/updateUser.service.ts
@@ -1,16 +1,21 @@
 import AppDataSource from "../../data-source";
+import Addresses from "../../entities/address.entity";
 import Users from "../../entities/user.entity";
 import AppError from "../../Error/AppError";
 import { IUserUpdate } from "../../interfaces/user";
 
 const updateUserService = async ({ name, email, password, age, sex, img, address }: IUserUpdate, userId: string): Promise<Users> => {
 	const userRepository = AppDataSource.getRepository(Users);
+	const addressesRepository = AppDataSource.getRepository(Addresses);
 	const findUser = await userRepository.findOneBy({ id: userId });
+	const addresses = await addressesRepository.find();
 	
 	if (!findUser) {
 		throw new AppError("User nor found", 404);
 	}
 
+	const addressUser = addresses.find(findedAddress => findedAddress.id === findUser.address.id)
+	
 	await userRepository.update(userId, {
 		name: name,
 		email: email,
@@ -18,8 +23,20 @@ const updateUserService = async ({ name, email, password, age, sex, img, address
 		age: age,
 		sex: sex,
 		img: img,
-		address: address
 	});
+
+	if (address) {
+		if (!addressUser) {
+			throw new AppError("Address not found", 404);
+		}
+		const updateAddress = await addressesRepository.update(addressUser.id, {
+			city: address.city,
+			district: address.district,
+			number: address.number,
+			zipCode: address.zipCode,
+			state: address.state
+		})
+	}
 
 	const user = await userRepository.findOne({
 		where: {
