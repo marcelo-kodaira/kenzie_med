5d59ed9e0216c6a45b9b18f0f5a0bfaf3d2ee688
Frederico Costa <costafredericob@gmail.com>
2022-11-04 10:22:57 -0300

    fix/ deploy config ajustes no path dos arquivos

--- src/data-source.ts
+++ src/data-source.ts
@@ -23,11 +23,11 @@ const AppDataSource = new DataSource(
 
         entities:
           process.env.NODE_ENV === "production"
-            ? ["dist/entities/*.js"]
+            ? ["dist/src/entities/*.js"]
             : ["src/entities/*.ts"],
         migrations:
           process.env.NODE_ENV === "production"
-            ? ["dist/migrations/*.js"]
+            ? ["dist/src/migrations/*.js"]
             : ["src/migrations/*.ts"],
       }
 );
