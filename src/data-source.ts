c151d2c159ffc195d5ffb4d8c79544711ac8239d
Frederico Costa <costafredericob@gmail.com>
2022-11-07 15:40:39 -0300

    fix/ deploy config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
-
+import path from "path";
+const isProduction = process.env.NODE_ENV === "production";
 const AppDataSource = new DataSource(
   process.env.NODE_ENV === "test"
     ? {
@@ -12,23 +13,22 @@ const AppDataSource = new DataSource(
     : {
         type: "postgres",
         url: process.env.DATABASE_URL,
-        ssl:
-          process.env.NODE_ENV === "production"
-            ? {
-                rejectUnauthorized: false,
-              }
-            : false,
+        ssl: isProduction
+          ? {
+              rejectUnauthorized: false,
+            }
+          : false,
         synchronize: false,
-        logging: true,
+        logging: isProduction ? false : true,
 
-        entities:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/entities/*.js"]
-            : ["src/entities/*.ts"],
-        migrations:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/migrations/*.js"]
-            : ["src/migrations/*.ts"],
+        entities: [path.join(__dirname, "./entities/*.{js,ts}")],
+        // process.env.NODE_ENV === "production"
+        //   ? ["dist/src/entities/*.js"]
+        //   : ["src/entities/*.ts"],
+        migrations: [path.join(__dirname, "./migrations/*.{js,ts}")],
+        // process.env.NODE_ENV === "production"
+        //   ? ["dist/src/migrations/*.js"]
+        //   : ["src/migrations/*.ts"],
       }
 );
 
