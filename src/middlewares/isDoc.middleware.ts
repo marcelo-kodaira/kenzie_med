f7af7b4b2c5723d9b638b1a91dd9640f02c15b3e
Frederico Costa <costafredericob@gmail.com>
2022-11-07 14:07:01 -0300

    fix/ conflitos em is Doc

--- src/middlewares/isDoc.middleware.ts
+++ src/middlewares/isDoc.middleware.ts
@@ -1,23 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../Error/AppError";
-<<<<<<< HEAD
-const isDocMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  if (req.user.crm) {
-    next();
-  }
 
-  throw new AppError("You need to be a doctor to acess this route", 401);
+const isDocMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user.crm && !req.user.isAdmin) {
+    throw new AppError("You need to be a doctor to access this route", 401);
+  }
+  next();
 };
 export default isDocMiddleware;
-=======
-
-const isDocMiddleware = (req:Request, res:Response, next:NextFunction) => {   
-
-    if(!req.user.crm  && !req.user.isAdmin){
-        throw new AppError("You need to be a doctor to access this route", 401)
-    }
-     next()    
-
-}
-export default isDocMiddleware
->>>>>>> tests/doctors
