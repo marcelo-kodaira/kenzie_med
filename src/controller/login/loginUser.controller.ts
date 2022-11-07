654156d6a74247d7ea7c405b6d828b72641c0711
Marcelo Almeida <92169034+marcelo-kodaira@users.noreply.github.com>
2022-11-07 09:43:26 -0300

    Update loginUser.controller.ts

--- src/controller/login/loginUser.controller.ts
+++ src/controller/login/loginUser.controller.ts
@@ -4,7 +4,6 @@ import loginUserService from "../../services/login/loginUser.service"
 
 const loginUserController = async (req: Request, res: Response) =>{
 
-    console.log(req.body)
     const data:ILogin = req.body
     const token = await loginUserService(data)
     return res.status(200).json({ token })
