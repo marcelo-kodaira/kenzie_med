c9a4ebf8fd035bb036ddd5c2aae2f5198a6e76b9
Frederico Costa <costafredericob@gmail.com>
2022-11-07 10:06:08 -0300

    test/ doctors

--- src/services/schedules/softDeleteSchedule.service.ts
+++ src/services/schedules/softDeleteSchedule.service.ts
@@ -1,32 +1,22 @@
-import AppDataSource from "../../data-source"
-import Schedules from "../../entities/schedule.entity"
-import Users from "../../entities/user.entity"
-import AppError from "../../Error/AppError"
-import { IscheduleEdit } from "../../interfaces/schedule"
+import AppDataSource from "../../data-source";
+import Schedules from "../../entities/schedule.entity";
+import Users from "../../entities/user.entity";
+import AppError from "../../Error/AppError";
+import { IscheduleEdit } from "../../interfaces/schedule";
 
 const softDeleteScheduleService = async (scheduleID: string) => {
+  const scheduleRepository = AppDataSource.getRepository(Schedules);
+  const userRepository = AppDataSource.getRepository(Users);
 
-    const scheduleRepository = AppDataSource.getRepository(Schedules)
-    const userRepository = AppDataSource.getRepository(Users)
+  const schedule = await scheduleRepository.findOneBy({
+    id: scheduleID,
+  });
 
-    const schedule = await scheduleRepository.findOneBy({
-        id: scheduleID
-    })
+  if (!schedule) {
+    throw new AppError("Schedule not found", 404);
+  }
 
-    if (!schedule) {
-        throw new AppError("Schedule not found", 404)
-    }
+  return schedule;
+};
 
-
-    await scheduleRepository.update(
-        scheduleID,
-        {
-            userId = null,
-            isAvailable = true
-        }
-    )
-
-    return schedule
-}
-
-export default softDeleteScheduleService
+export default softDeleteScheduleService;
