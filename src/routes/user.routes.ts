8fb94f8d1fe0a4ee443d0f7a38960b5dc9a55d04
Daniel Galvan <dgalvan@gmail.com>
2022-11-08 13:06:36 -0300

    fix: Ticket-006 - Adição middleware isOwner.middle na rota delete/users/:id, e adição de testes na rota user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -9,18 +9,14 @@ import listAllUsersController from "../controller/users/listAllUsers.controller"
 import retrieveUserController from "../controller/users/retrieveUser.controller";
 import updateUserController from "../controller/users/updateUser.controller";
 import disableUserController from "../controller/users/disableUser.constroller";
+import isOwnerMiddleware from "../middlewares/isOwner.middleware";
 
 const userRoutes = Router();
 
 userRoutes.post("", creteUserController);
 userRoutes.get("", authMiddleware, isAdmMiddleware, listAllUsersController);
 userRoutes.get("/profile", authMiddleware, retrieveUserController);
-userRoutes.patch(
-  "/:id",
-  authMiddleware,
-  verifyUpdateRequestUserMiddleware,
-  updateUserController
-);
-userRoutes.delete("/:id", authMiddleware, disableUserController);
+userRoutes.patch("/:id", authMiddleware, verifyUpdateRequestUserMiddleware, updateUserController);
+userRoutes.delete("/:id", authMiddleware, isOwnerMiddleware, disableUserController);
 
 export default userRoutes;
