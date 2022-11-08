27cfc3f5606d2bc7e600ea10a41613484fe05c82
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2022-11-08 09:04:19 -0300

    Feat: Ticket011-LoginTests

--- src/__tests__/integration/login/loginUserRoutes.tests.ts
+++ src/__tests__/integration/login/loginUserRoutes.tests.ts
@@ -3,7 +3,31 @@ import AppDataSource from "../../../data-source";
 import request from "supertest"
 import app from "../../../app";
 import { mockedAdmin, mockedUser, mockedUserActive, mockedUserActiveLogin, mockedUserLogin } from "../../mocks/users";
+import { ILogin } from "../../../interfaces/login";
+import { IUserRequest } from "../../../interfaces/user";
 
+const mockedInactiveUser: IUserRequest = {
+    name: "Daniel",
+    email: "marcelo@mail.com",
+    isAdmin: false,
+    password: "abcdefg",
+    age: 30,
+    CPF: "12345678911",
+    sex: "Masculino",
+    img: "htttp://",
+    address: {
+        district: "Rua São Vicente",
+        city: "São Paulo",
+        number: 89,
+        state: "SP",
+        zipCode: "121556"
+    }
+}
+
+const mockedInactiveUserLogin: ILogin = {
+    email: "marcelo@mail.com",
+    password: "abcdefg"
+}
 
 
 describe("/login", () => {
@@ -16,8 +40,8 @@ describe("/login", () => {
             console.error("Error during Data Source initialization", err)
         })
 
-        const response = await request(app).post('/users').send(mockedUser)
-        await request(app).delete(`/users/${response.body.id}`).send(mockedUser)
+
+        
 
         await request(app).post('/users').send(mockedUserActive)
         await request(app).post('/users').send(mockedAdmin)
@@ -28,7 +52,7 @@ describe("/login", () => {
     })
 
     test("POST /login -  should be able to login with the user",async () => {
-        const response = await request(app).post("/login/users").send(mockedUserActiveLogin);
+        const response = await request(app).post("/login/users").send(mockedAdmin);
         
         expect(response.body).toHaveProperty("token")
         expect(response.status).toBe(200)
@@ -58,7 +82,10 @@ describe("/login", () => {
     })
 
     test("POST /login -  Should not be able to login with  user with isActive = false",async () => {
-        const response = await request(app).post("/login/users").send(mockedUserLogin);
+        const res = await request(app).post('/users').send(mockedInactiveUser)
+        await request(app).delete(`/users/${res.body.id}`).send(mockedInactiveUser)
+
+        const response = await request(app).post("/login/users").send(mockedInactiveUserLogin);
 
         expect(response.body).toHaveProperty("message")
         expect(response.status).toBe(403)
