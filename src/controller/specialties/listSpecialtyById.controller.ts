eba132aa5c5143fa291e15a1c354e1d64f30801a
Daniel Galvan <dgalvan@gmail.com>
2022-11-09 08:31:36 -0300

    Fix: Nomenclatura controller listSpecialtyById

--- src/controller/specialties/listSpecialtyById.controller.ts
+++ src/controller/specialties/listSpecialtyById.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express"
 import { ISpecialtyRequest } from "../../interfaces/specialty"
 import listSpecialtyByIdService from "../../services/specialties/listSpecialtyById.service"
 
-const ListPropertiesController = async (req: Request, res: Response) => {
+const ListSpecialtyByIdController = async (req: Request, res: Response) => {
   const id = req.params.id
 
   const specialtiesList: ISpecialtyRequest = await listSpecialtyByIdService(id)
@@ -10,4 +10,4 @@ const ListPropertiesController = async (req: Request, res: Response) => {
   return res.status(200).send(specialtiesList)
 }
 
-export default ListPropertiesController
+export default ListSpecialtyByIdController
